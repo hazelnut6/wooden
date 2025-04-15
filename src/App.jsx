032d72265e03197fb11f3a20e5f62ad9b8dd9f9f
hazelnut6 <hazelpachoco6@gmail.com>
2025-04-15 12:46:26 +0800

    Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-
+import { NavLink } from 'react-router-dom'
+import Nav from './components/Nav'
+import './styles/app.css'
 
 function App() {
 
 
   return (
-    <h1>Home</h1>
+    <div className='homeDiv'>
+      <nav>
+        <Nav />
+      </nav>
+      <div className="homeContent"> 
+        <h1 className='webName'>Wooden.</h1>
+        <button className='shopBtn'>
+          <NavLink to='/categories' className='shopBtnLink'>Shop now</NavLink>
+        </button>
+      </div>
+    </div>
   )
 }
 
